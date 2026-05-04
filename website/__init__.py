fe36ec7e0f756723e740488b4f9406aa24687e87
Ahmad_Andika <andika.work.1111@gmail.com>
2026-05-04 17:57:03 +0700

    push to 26

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,8 @@ def create_app():
 
     # app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:@localhost/database_smpn_287"
     # app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://andika:{quote_plus(os.getenv('DB_PASSWORD'))}@187.77.113.166/{os.getenv('DB_NAME')}"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:{quote_plus(os.getenv('DB_PASSWORD'))}@localhost/{os.getenv('DB_NAME')}"
+    # app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:{quote_plus(os.getenv('DB_PASSWORD'))}@localhost/{os.getenv('DB_NAME')}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqlconnector://root:{quote_plus(os.getenv('DB_PASSWORD'))}@202.155.19.242/{os.getenv('DB_NAME')}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
