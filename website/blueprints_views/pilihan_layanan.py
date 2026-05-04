195ce467edf3913fda8139386e9d5de975d8fadc
Ahmad_Andika <andika.work.1111@gmail.com>
2026-05-04 14:29:28 +0700

    push to 26

--- website/blueprints_views/pilihan_layanan.py
+++ website/blueprints_views/pilihan_layanan.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
-from flask_login import current_user
-
 views = Blueprint("pilihan_layanan", __name__)
 
 @views.route("/pilihan-layanan")
 def pilihan_layanan():
-    return render_template("pilihan-layanan.html", user=current_user)
+    return render_template("pilihan-layanan.html")
