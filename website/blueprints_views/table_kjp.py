0366774dbb3e38bbe1c7d443863c9b1e65bec59d
Ahmad_Andika <andika.work.1111@gmail.com>
2026-05-14 11:56:36 +0700

    fixed banyak pusing .🥲🥲🥲🥲

--- website/blueprints_views/table_kjp.py
+++ website/blueprints_views/table_kjp.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from..models import DatabaseLayananKjp
+from flask_login import login_required
+from ..views import role_required
 
 views = Blueprint("table_kjp", __name__)
 
 @views.route("/table-kjp", methods=["GET", "POST"])
+@login_required
 def table_kjp():
     data = DatabaseLayananKjp.query.all()
     return render_template("table-kjp.html", data=data)
