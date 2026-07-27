3d2e2b2ffc0c732a3faa8d380b1aea6609a21ba8
AhmadAndika2011 <ahmad.andika.training1.0@gmail.com>
2026-07-28 03:27:18 +0700

    'test Update lagi'

--- website/blueprints/layanan_kjp_baru.py
+++ website/blueprints/layanan_kjp_baru.py
@@ -17,36 +17,71 @@ auth = Blueprint("layanan_kjp_baru", __name__)
 def layanan_kjp_baru():
     if request.method == "POST":
         #? NON GAMBAR
+        pemohon_rt_rw = request.form.get("pemohon_rt_rw").strip()
+        check_pemohon_rt_rw = pemohon_rt_rw.split("/")
+        if len(check_pemohon_rt_rw) != 2:
+            flash("Pemohon Rt/Rw tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+        
+        pemohon_kode_pos = request.form.get("pemohon_kode_pos").strip()
+        if len(pemohon_kode_pos) != 5:
+            flash("Pemohon Kode Pos tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+
+        pemohon_telpon = request.form.get("pemohon_telpon").strip()
+        if len(pemohon_telpon) > 13 and len(pemohon_telpon) < 10:
+            flash("Pemohon No telepon tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+        
+        peserta_didik_rt_rw = request.form.get("peserta_didik_rt_rw")
+        check_peserta_didik_rt_rw = peserta_didik_rt_rw.split("/")
+        if len(check_peserta_didik_rt_rw) != 2:
+            flash("Peserta didik Rt/Rw tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+        
+        peserta_didik_kode_pos = request.form.get("peserta_didik_kode_pos").strip()
+        if len(peserta_didik_kode_pos) != 5:
+            flash("Peserta didik Kode Pos tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+        
+        sekolah_rt_rw = request.form.get("sekolah_rt_rw")
+        check_sekolah_rt_rw = sekolah_rt_rw.split("/")
+        if len(check_sekolah_rt_rw) != 2:
+            flash("Peserta didik Rt/Rw tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+
+        sekolah_kode_pos = request.form.get("sekolah_kode_pos").strip()
+        if len(sekolah_kode_pos) != 5:
+            flash("Sekolah Kode Pos tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+        
+        pernyataan_nisn = request.form.get("pernyataan_nisn").strip()
+        if len(pernyataan_nisn) != 50:
+            flash("Pernyataan NISN tidak valid", category="error")
+            return redirect(url_for("layanan_kjp_baru.layanan_kjp_baru"))
+
         fields = {
         "tanggal_surat" : request.form.get("tanggal_surat"),
         "pemohon_nama" : request.form.get("pemohon_nama"),
         "pemohon_alamat" : request.form.get("pemohon_alamat"),
-        "pemohon_rt_rw" : request.form.get("pemohon_rt_rw"),
         "pemohon_kelurahan" : request.form.get("pemohon_kelurahan"),
         "pemohon_kecamatan" : request.form.get("pemohon_kecamatan"),
         "pemohon_kota" : request.form.get("pemohon_kota"),
-        "pemohon_kode_pos" : request.form.get("pemohon_kode_pos"),
-        "pemohon_telpon" : request.form.get("pemohon_telpon"),
         "peserta_didik_nama" : request.form.get("peserta_didik_nama"),
         "peserta_didik_tempat_lahir" : request.form.get("peserta_didik_tempat_lahir"),
         "peserta_didik_tanggal_lahir" : request.form.get("peserta_didik_tanggal_lahir"),
         "peserta_didik_jenis_kelamin" : request.form.get("peserta_didik_jenis_kelamin"),
         "peserta_didik_alamat" : request.form.get("peserta_didik_alamat"),
-        "peserta_didik_rt_rw" : request.form.get("peserta_didik_rt_rw"),
         "peserta_didik_kelurahan" : request.form.get("peserta_didik_kelurahan"),
         "peserta_didik_kecamatan" : request.form.get("peserta_didik_kecamatan"),
         "peserta_didik_kota" : request.form.get("peserta_didik_kota"),
-        "peserta_didik_kode_pos" : request.form.get("peserta_didik_kode_pos"),
         "sekolah_nama" : request.form.get("sekolah_nama"),
         "sekolah_alamat" : request.form.get("sekolah_alamat"),
-        "sekolah_rt_rw" : request.form.get("sekolah_rt_rw"),
         "sekolah_kelurahan" : request.form.get("sekolah_kelurahan"),
         "sekolah_kecamatan" : request.form.get("sekolah_kecamatan"),
         "sekolah_kota" : request.form.get("sekolah_kota"),
-        "sekolah_kode_pos" : request.form.get("sekolah_kode_pos"),
         "ttd_nama_lengkap" : request.form.get("ttd_nama_lengkap"),
         "pernyataan_nama_peserta_didik" : request.form.get("pernyataan_nama_peserta_didik"),
-        "pernyataan_nisn" : request.form.get("pernyataan_nisn"),
         "pernyataan_kelas" : request.form.get("pernyataan_kelas"),
         "pernyataan_sekolah" : request.form.get("pernyataan_sekolah"),
         "pernyataan_nama_orang_tua" : request.form.get("pernyataan_nama_orang_tua"),
@@ -122,35 +157,35 @@ def layanan_kjp_baru():
             tanggal_surat = fields["tanggal_surat"],
             pemohon_nama = fields["pemohon_nama"],
             pemohon_alamat = fields["pemohon_alamat"],
-            pemohon_rt_rw = fields["pemohon_rt_rw"],
+            pemohon_rt_rw = pemohon_rt_rw,
             pemohon_kelurahan = fields["pemohon_kelurahan"],
             pemohon_kecamatan = fields["pemohon_kecamatan"],
             pemohon_kota = fields["pemohon_kota"],
-            pemohon_kode_pos = fields["pemohon_kode_pos"],
-            pemohon_telpon = fields["pemohon_telpon"],
+            pemohon_kode_pos = pemohon_kode_pos,
+            pemohon_telpon = pemohon_telpon,
             peserta_didik_nama = fields["peserta_didik_nama"],
             peserta_didik_tempat_lahir = fields["peserta_didik_tempat_lahir"],
             peserta_didik_tanggal_lahir = fields["peserta_didik_tanggal_lahir"],
             peserta_didik_jenis_kelamin = fields["peserta_didik_jenis_kelamin"],
             peserta_didik_alamat = fields["peserta_didik_alamat"],
-            peserta_didik_rt_rw = fields["peserta_didik_rt_rw"],
+            peserta_didik_rt_rw = peserta_didik_rt_rw,
             peserta_didik_kelurahan = fields["peserta_didik_kelurahan"],
             peserta_didik_kecamatan = fields["peserta_didik_kecamatan"],
             peserta_didik_kota = fields["peserta_didik_kota"],
-            peserta_didik_kode_pos = fields["peserta_didik_kode_pos"],
+            peserta_didik_kode_pos = peserta_didik_kode_pos,
             sekolah_nama = fields["sekolah_nama"],
             sekolah_alamat = fields["sekolah_alamat"],
-            sekolah_rt_rw = fields["sekolah_rt_rw"],
+            sekolah_rt_rw = sekolah_rt_rw,
             sekolah_kelurahan = fields["sekolah_kelurahan"],
             sekolah_kecamatan = fields["sekolah_kecamatan"],
             sekolah_kota = fields["sekolah_kota"],
-            sekolah_kode_pos = fields["sekolah_kode_pos"],
+            sekolah_kode_pos = sekolah_kode_pos,
             lampiran_kk = lampiran_kk,
             lampiran_ktp = lampiran_ktp,
             ttd_nama_lengkap = fields["ttd_nama_lengkap"],
             ttd_tanda_tangan = ttd_tanda_tangan,
             pernyataan_nama_peserta_didik = fields["pernyataan_nama_peserta_didik"],
-            pernyataan_nisn = fields["pernyataan_nisn"],
+            pernyataan_nisn = pernyataan_nisn,
             pernyataan_kelas = fields["pernyataan_kelas"],
             pernyataan_sekolah = fields["pernyataan_sekolah"],
             pernyataan_nama_orang_tua = fields["pernyataan_nama_orang_tua"],
